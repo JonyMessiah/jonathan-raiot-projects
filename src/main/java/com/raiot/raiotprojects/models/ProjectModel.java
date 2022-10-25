6b3712eeb774f45dabc52ccdadb3dfb1daf31beb
Jonytheus <109642412+JonyMessiah@users.noreply.github.com>
2022-10-24 20:02:45 -0600

    feat: #1 DAO for projects

---
+++ src/main/java/com/raiot/raiotprojects/models/ProjectModel.java
@@ -0,0 +1,44 @@
+package com.raiot.raiotprojects.models;
+
+public class ProjectModel {
+
+   private String name;
+   private String category;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    private String created_at;
+   private String updated_at;
+
+
+}
--- src/main/java/com/raiot/raiotprojects/models/UserModel.java
+++ src/main/java/com/raiot/raiotprojects/models/UserModel.java
@@ -2,6 +2,50 @@ package com.raiot.raiotprojects.models;
 
 public class UserModel {
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
     // Needed for login, #9
     private String email;
     private String password;
