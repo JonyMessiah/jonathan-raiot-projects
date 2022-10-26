f75a9ee5258e1858cc7234042e87945c6d78504a
Enrique Mendez <brauliomendez@gmail.com>
2022-10-26 14:16:51 -0600

    feat: login working

--- src/main/java/com/raiot/raiotprojects/RaiotProjectsApplication.java
+++ src/main/java/com/raiot/raiotprojects/RaiotProjectsApplication.java
@@ -8,6 +8,9 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class RaiotProjectsApplication extends Application {
+
+    public static  Integer user_id;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(
--- src/main/java/com/raiot/raiotprojects/controllers/LoginController.java
+++ src/main/java/com/raiot/raiotprojects/controllers/LoginController.java
@@ -2,6 +2,7 @@ package com.raiot.raiotprojects.controllers;
 
 import com.raiot.raiotprojects.RaiotProjectsApplication;
 import com.raiot.raiotprojects.dao.SQLiteDao;
+import com.raiot.raiotprojects.models.UserModel;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,11 +10,17 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class LoginController {
+    @FXML
+    Label label_Error;
 
     @FXML
     Button btn_Login;
@@ -22,13 +29,52 @@ public class LoginController {
     Hyperlink hyperlink_Register;
 
     @FXML
+    TextField field_Email;
+
+    @FXML
+    TextField field_Password;
+
+    @FXML
     protected void onLoginButtonClick() throws Exception {
+        label_Error.setVisible(false);
         SQLiteDao sqlite = new SQLiteDao();
         Connection connection = sqlite.getConnection();
 
-        Parent registerScene = FXMLLoader.load(RaiotProjectsApplication.class.getResource("home.fxml"));
-        Stage window = (Stage) btn_Login.getScene().getWindow();
-        window.setScene(new Scene(registerScene));
+        String email = field_Email.getText();
+        String password = field_Password.getText();
+
+        // TODO validate if emails contains values, if not display label
+
+        String query = "SELECT id, password, name, lastname FROM users WHERE  email = ?";
+
+        PreparedStatement pstmt  = connection.prepareStatement(query);
+        pstmt.setString(1, email);
+        ResultSet rs    = pstmt.executeQuery();
+
+        boolean exists = false;
+        System.out.println(email);
+        while(rs.next()) {
+            String user_password = rs.getString("password");
+            if (user_password.equals(password)) {
+                // login user
+                RaiotProjectsApplication.user_id = rs.getInt("id");
+
+                Parent registerScene = FXMLLoader.load(RaiotProjectsApplication.class.getResource("home.fxml"));
+                Stage window = (Stage) btn_Login.getScene().getWindow();
+                window.setScene(new Scene(registerScene));
+                exists = true;
+            }
+            break;
+        }
+
+        if (!exists) {
+            // Error
+            label_Error.setVisible(true);
+        }
+
+
+
+
     }
 
     @FXML
